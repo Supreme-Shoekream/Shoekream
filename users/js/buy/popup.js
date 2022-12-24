a6f5ba1e7ea898cd15850d3beb835513252a3f72
jooeun <jje320594@gmail.com>
2022-12-25 00:00:55 +0900

    buySelect.html/css/js추가buyCheck.html,js수정 popup.css,js추가 팝업창, 버튼활성화, 체크박스 등 기능 구현

---
+++ users/js/buy/popup.js
@@ -0,0 +1,17 @@
+function pop_notice(){
+    document.querySelector('.layer_order_notice').style.display="block"
+}
+
+function closeLayer(){
+    document.querySelector('.layer_order_notice').style.display="none"
+    document.querySelector('.layer_auth_policy').style.display="none"
+}
+
+
+function pop_auth_policy(){
+    document.querySelector('.layer_auth_policy').style.display="block"
+}
+
+function pop_policy(){
+
+}
