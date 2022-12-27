3b6a5b063d7ca50b0df550101df36289a29b4fe3
juy <61074722+juyeon1570@users.noreply.github.com>
2022-12-27 17:08:08 +0900

    경로 충돌 해결

---
+++ users/js/login/join.js
@@ -0,0 +1,8 @@
+// 사이즈 팝업창
+function size_layer(){
+    document.querySelector('.size_layer').style.display="block"
+}
+
+function closeLayer(){
+    document.querySelector('.size_layer').style.display="none"
+}
