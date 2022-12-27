285ba83d1390ce37192ea5d94e82d0333b23f0d5
Sophie <sophiakim.dev@gmail.com>
2022-12-27 15:14:31 +0900

    join page js popup

--- users/js/login/join.js
+++ users/js/login/join.js
@@ -1,12 +1,4 @@
 // 사이즈 팝업창
-// $("#size_layer_open").on("click", function() {
-//     $(".size_layer").css('display','block');
-// });
-// $(".btn_layer_close").on("click", function() {
-//     $(".size_layer").css('display','none');
-// });
-
-
 function size_layer(){
     document.querySelector('.size_layer').style.display="block"
 }
