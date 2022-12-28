cdaf8631548970f0c6a9d78b83c570ff5b65b233
yunanjk <lyra082172@naver.com>
2022-12-29 01:33:13 +0900

    상품 상세 페이지 - product.js2 [버튼이벤트] - (모든 사이즈, 관심상품추가)

--- users/js/product.js
+++ users/js/product.js
@@ -1,23 +1,52 @@
-// 기능1. 스크롤시 왼쪽 상품 사진 움직이기
-const is_fixed= document.querySelector(".is_fixed");
-    // 왼쪽 상품 사진 ㅋ
 
-// const contentTop = is_fixed.getBoundingClientRect().top + window.scrollY; 
-    // 컨텐츠 영역부터 브라우저 최상단까지 길이 구함
-    // getBoundingClientRect().top: 뷰포트 상의 좌표값을 나타냄
-    // window.scrollY: 브라우저 최상단에서 현재까지 스크롤 된 좌표값 구함
+// 기능1. 스크롤시 왼쪽 상품 사진 이동후 멈춤
+const is_fixed= document.querySelector(".is_fixed");
+    // 왼쪽 상품 사진 
 
 window.addEventListener('scroll', function(){
     // console.log(window.scrollY);
-    if(window.scrollY >= 1200){ // 좌표값: 1200에서 멈춰야함
+    if(window.scrollY >= 1250){ 
+        // window.scrollY: 브라우저 최상단에서 현재까지 스크롤 된 좌표값 구함
+        // 좌표값이 1250인 곳에서 멈춰야함
         is_fixed.classList.add('is_absolute');
-            // css에서 position:absolute , top:1200px 지정해줬음
+            // css에서 position:absolute , top:1250px으로 지정해줬음
     }else{
         is_fixed.classList.remove('is_absolute');
     }
 });
 
 
+// 기능2. 모든 사이즈 옆 버튼 클릭시 팝업 레이어창 
+function everysizePopup() {
+    const layer = document.querySelector('.lg.layer_detail_size_select.select_only_size');
+    layer.classList.add('layer');
+    const body = document.querySelector('body');
+    body.style.overflow = 'hidden';
+}
+
+function everysizePopdown() {
+    const layer = document.querySelector('.lg.layer_detail_size_select.select_only_size');
+    layer.classList.remove('layer');
+    const body = document.querySelector('body');
+    body.style.overflow = '';
+
+}
+
+// 기능3. 
+function wishPopup() {
+    const layer = document.querySelector('.layer_interest.mo');
+    layer.classList.add('layer');
+    const body = document.querySelector('body');
+    body.style.overflow = 'hidden';
+}
+
+function wishPopdown() {
+    const layer = document.querySelector('.layer_interest.mo');
+    layer.classList.remove('layer');
+    const body = document.querySelector('body');
+    body.style.overflow = '';
+
+}
 
 
 
