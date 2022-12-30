0fa65458fff517e4dc69264c6535f3570d98cdaa
yunanjk <lyra082172@naver.com>
2022-12-30 10:24:48 +0900

    상품 상세 페이지 - product.js [스크롤시 상단 상품 정보 고정 배너1, 시세-체결내역1, 스타일쪽 html,css 변경(홈페이지업데이트됨) ]

--- users/js/product.js
+++ users/js/product.js
@@ -1,13 +1,18 @@
+// 화면 스크롤 좌표값 구하는 이벤트
+// window.addEventListener('scroll', function(){
+//     console.log(window.scrollY)
+// });
+
 
 // 기능1. 스크롤시 왼쪽 상품 사진 이동후 멈춤
 const is_fixed= document.querySelector(".is_fixed");
     // 왼쪽 상품 사진 
 
 window.addEventListener('scroll', function(){
-    // console.log(window.scrollY);
-    if(window.scrollY >= 1250){ 
+    console.log(window.scrollY);
+    if(window.scrollY >= 1240){ 
         // window.scrollY: 브라우저 최상단에서 현재까지 스크롤 된 좌표값 구함
-        // 좌표값이 1250인 곳에서 멈춰야함
+        // 좌표값이 1240인 곳에서 멈춰야함
         is_fixed.classList.add('is_absolute');
             // css에서 position:absolute , top:1250px으로 지정해줬음
     }else{
@@ -64,70 +69,67 @@ function wishPopdown() {
 }
 
 
-// 기능4. 시세 그래프
-// 개월 버튼 클릭하기
-const items = document.querySelectorAll(".tab_list .graph_month");
-    console.log(items);
-    //배열로 저장되기 때문에 forEach로 하나씩 이벤트를 등록해준다.
-    items.forEach((item)=>{
-
-        item.addEventListener('click',()=>{
-            items.forEach((e)=>{
-            //하나만 선택되도록 기존의 효과를 지워준다.
-                e.classList.remove('on');
-                e.setAttribute('aria-selected','false');
-            })
-            
-        // 선택한 그 아이만 효과를 추가해준다.
-            item.classList.add('on');
-            item.setAttribute('aria-selected','true');
-            
-            // const a = document.querySelector('.tab_list .graph_month');
-            // const b = document.getElementById(a.getAttribute('aria-controls'));
-            // console.log(b);
-
-            // const ch = b.childNodes;
-
-            // const canvas = ch[0].childNodes;
-
-            // const k = canvas[0];
-            // console.log(k.getAttribute('id'));
+// 기능4. 시세 그래프 - 개월 버튼
+const dates = document.querySelectorAll(".tab_list .date");
+// console.log(items);
+//배열로 저장되기 때문에 forEach로 하나씩 이벤트를 등록해준다.
+dates.forEach((date)=>{
+    date.addEventListener('click',()=>{
+        dates.forEach((e)=>{
+        //하나만 선택되도록 기존의 효과를 지워준다.
+            e.classList.remove('on');
+            e.setAttribute('aria-selected','false');
         })
-    })
+        
+    // 선택한 그 아이만 효과를 추가해준다.
+        date.classList.add('on');
+        date.setAttribute('aria-selected','true');
+        
+        // const a = document.querySelector('.tab_list .graph_month');
+        // const b = document.getElementById(a.getAttribute('aria-controls'));
+        // console.log(b);
 
-// 개월 버튼 클릭시 그래프 띄우기
+        // const ch = b.childNodes;
+
+        // const canvas = ch[0].childNodes;
+
+        // const k = canvas[0];
+        // console.log(k.getAttribute('id'));
+    })
+});
+
+// 그래프 띄우기
     // const a = document.querySelector('.tab_list .graph_month');
     // const b = document.getElementById(a.getAttribute('aria-controls'));
-
-    const sales_panel1 = document.getElementById("graph1m").getContext("2d");
-    const graph1m = new Chart(sales_panel1, {
-        type: "line",
-        data: {
-            labels: [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
-            datasets: [{
-                label: "시세",
-                data: [784000, 784000, 789000, 858000, 792000, 790000, 100000, 200000, 320000, 9000],
-                backgroundColor: 'rgba(255, 99, 132, 0.2)',
-                borderColor: 'rgba(255, 99, 132, 1)',
-                borderWidth: 1
-            }]
-        },
-        options: {
-            responsive: false,
-            scales: {
-                y: {
-                    beginAtZero: false,
-                    position: 'right' // 축이 왼쪽에 표시될지, 오른쪽에 표시될지 정할 수 있습니다.
-                }
-            },
-            plugins: {
-                legend: {
-                    display: false // 라벨 없애기(그래프제목)
-                }
+const sales_panel1 = document.getElementById("graph1m").getContext("2d");
+const graph1m = new Chart(sales_panel1, {
+    type: "line",
+    data: {
+        labels: [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+        datasets: [{
+            label: "시세",
+            data: [784000, 784000, 789000, 858000, 792000, 790000, 100000, 200000, 320000, 9000],
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            borderColor: 'rgba(255, 99, 132, 1)',
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: false,
+        scales: {
+            y: {
+                beginAtZero: false,
+                position: 'right' // 축이 왼쪽에 표시될지, 오른쪽에 표시될지 정할 수 있습니다.
             }
         },
-        
-    });
+        plugins: {
+            legend: {
+                display: false // 라벨 없애기(그래프제목)
+            }
+        }
+    },
+    
+});
 
     // const monthOn = document.querySelector('#sales_panel1');
     // monthOn.classList.add('layer');
@@ -138,12 +140,94 @@ const items = document.querySelectorAll(".tab_list .graph_month");
     // layer.style.visibility ='visible';
 
 
+// 기능5. 시세 - 체결내역
+const conclusiones = document.querySelectorAll(".tab_list .conclusion");
+conclusiones.forEach((conclusion)=>{
+    conclusion.addEventListener("click",() => {
+        conclusiones.forEach((e) => {
+            e.classList.remove("on");
+            e.setAttribute("aria-selected","false");
+        })
+    conclusion.classList.add("on");
+    conclusion.setAttribute("aria-selected","true");
+    })
+});
+
+// 기능6. 시세 - 모든 사이즈
+// function graphEverysizePopup() {
+//     // 열기
+//     // const select1 = document.querySelector(".btn.btn_select");
+//     // select1.classList.add("on");
+    
+//     // const layer = document.querySelector(".layer_size_list.layer.lg");
+//     // layer.style.visibility = "visible";
+
+//     // // 닫기
+//     // const select2 = document.querySelector(".btn.btn_select.on");
+//     // select2.classList.remove("on");
+
+//     // const layer2 = document.querySelector(".layer_size_list.layer.lg");
+//     // layer2.style.visibility ="hidden";
 
 
 
+//     const select = document.querySelector(".filter_unit");
+//     console.log(select);
+
+//     select.addEventListener("click", () => {
+//         const layer = document.querySelector(".layer_size_list.layer.lg");
+//         if(layer.classList.add("on")){
+//             layer.style.visibility = "visible";
+//         }else{
+//             layer.classList.remove("on");
+//             layer.style.visibility = "hidden";
+//         }
+//     });
+// }
+
+const select = document.querySelector(".filter_unit .btn.btn_select");
+console.log(select);
+
+select.addEventListener("click", () => {
+    const layer = document.querySelector(".layer_size_list.layer.lg");
+    console.log(layer);
+
+    select((e) => {
+        e.classList.remove("on");
+        layer.style.visibility = "hidden";
+    })
+    select.classList.add("on");
+    layer.style.visibility = "visible";
+
+    // if(layer.getAttribute("class","on")){
+    //     // layer.classList.remove("on");
+    //     // layer.classList.add("on")
+    //     layer.removeAttribute("class","layer_size_list lg");
+    //     // layer.style.visibility = "visible";
+    // }else{
+    //     layer.setAttribute("class","layer_size_list lg");
+    //     // layer.classList.remove("on");
+    //     // layer.style.visibility = "hidden";
+    // }
+});
 
 
 
+// 기능7. 스크롤시 상단 고정 배너
+// 360
 
+const floating = document.querySelector(".floating_price.lg");
+
+window.addEventListener("scroll", function(){
+    
+    if(window.scrollY >= 350){ 
+        // window.scrollY: 브라우저 최상단에서 현재까지 스크롤 된 좌표값 구함
+        // 좌표값이 1250인 곳에서 멈춰야함
+        floating.classList.add("is_open");
+            // css에서 position:absolute , top:1250px으로 지정해줬음
+    }else{
+        floating.classList.remove("is_open");
+    }
+});
 
 
