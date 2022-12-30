62616d841645c9117133596fc6bd0c764856ac20
yunanjk <lyra082172@naver.com>
2022-12-30 14:05:18 +0900

    상품 상세 정보 - product.js [구매전확인사항 드롭다운창1]

--- users/js/product.js
+++ users/js/product.js
@@ -8,8 +8,11 @@
 const is_fixed= document.querySelector(".is_fixed");
     // 왼쪽 상품 사진 
 
+// const y = document.querySelector(".colomn");
+// const y2 = window.pageYOffset + y.getBoundingClientRect().top;
+
 window.addEventListener('scroll', function(){
-    console.log(window.scrollY);
+    // console.log(window.scrollY);
     if(window.scrollY >= 1240){ 
         // window.scrollY: 브라우저 최상단에서 현재까지 스크롤 된 좌표값 구함
         // 좌표값이 1240인 곳에서 멈춰야함
@@ -19,6 +22,7 @@ window.addEventListener('scroll', function(){
         is_fixed.classList.remove('is_absolute');
     }
 });
+
 
 
 // 기능2. 모든 사이즈 클릭시 팝업 레이어창 
@@ -214,20 +218,31 @@ select.addEventListener("click", () => {
 
 
 // 기능7. 스크롤시 상단 고정 배너
-// 360
-
 const floating = document.querySelector(".floating_price.lg");
-
 window.addEventListener("scroll", function(){
     
     if(window.scrollY >= 350){ 
-        // window.scrollY: 브라우저 최상단에서 현재까지 스크롤 된 좌표값 구함
-        // 좌표값이 1250인 곳에서 멈춰야함
         floating.classList.add("is_open");
-            // css에서 position:absolute , top:1250px으로 지정해줬음
     }else{
         floating.classList.remove("is_open");
     }
 });
 
 
+// 기능8. 구매전확인사항
+const dropdowns = document.querySelectorAll(".dropdown");
+dropdowns.forEach((dropdown)=>{
+
+    dropdown.addEventListener("click",() => {
+
+        const ch = dropdown.childNodes;
+        console.log(ch);
+
+        if(ch[3].getAttribute("class", "dc")){
+            ch[3].removeAttribute("class", "dc");
+        }else{
+            ch[3].setAttribute("class", "dc");
+        }
+
+    })
+});
