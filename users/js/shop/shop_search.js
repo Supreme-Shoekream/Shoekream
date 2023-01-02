22c1a8bc7f27a059594bb10bbc22b38edebff489
ildong2e <rlaehddlfzzz@gmail.com>
2023-01-02 10:06:24 +0900

    shop-search 수정

--- users/js/shop/shop_search.js
+++ users/js/shop/shop_search.js
@@ -107,6 +107,105 @@ function dd_menu8() {
     }
 }
 
+// 탑 메뉴 클릭시 효과
+const quickbtn = document.querySelectorAll(".btn_quick1");
+
+function handleClick(event) {
+// console.log(event.target.classList);
+  // console.log(this);
+  // 콘솔창을 보면 둘다 동일한 값이 나온다
+
+
+if (event.target.classList[2] == "clicked") {
+    event.target.classList.remove("clicked");
+} else {
+    for (let i = 0; i < quickbtn.length; i++) {
+    quickbtn[i].classList.remove("clicked");
+    }
+
+    event.target.classList.add("clicked");
+}
+}
+
+function init() {
+for (let i = 0; i < quickbtn.length; i++) {
+    quickbtn[i].addEventListener("click", handleClick);
+}
+}
+
+init();
+
+
+// console.log(document.querySelectorAll('.btn_quick1'))
+
+
+
+// let catecheck2 = document.querySelectorAll('.checkInput');
+// let checkImg = document.querySelectorAll(".checkImg");
+// $(document).on('click', '.btn_quick_filter_js', function (e) {
+//     e.preventDefault();
+//     let index = $(".btn_quick_filter_js").index(this);
+//     if (catecheck2[index].checked == false) {
+//         catecheck2[index].checked = true;
+//         $('.top_box').eq(index + 1).css({"display": "block"});
+//         $('.top_box').not($('.top_box').eq(index + 1)).css({"display": "none"});
+//         checkImg[index].src = "/lib/img/check_box_b.png";
+//         for (let i in checkImg) {
+//             if (i != index) {
+//                 checkImg[i].src = "/lib/img/check_box.png";
+//                 catecheck2[i].checked = false;
+//             }
+//         }
+//         $(this).css({"background-color": "rgb(250, 221, 221)", "color": "rgb(252, 121, 121)"});
+
+//         $('.btn_quick_filter_js').not($(this)).css({
+//             "background-color": "rgb(240, 240, 240)",
+//             "color": "black"
+//         });
+
+//         console.log(catecheck2[index])
+//         let $save_check = catecheck2[index]
+//         let $cate_sell = $save_check.value;
+//         click_you = $cate_sell;
+
+
+//     } else if (catecheck2[index].checked == true) {
+//         catecheck2[index].checked = false;
+//         $('.top_box').eq(0).css({"display": "block"});
+//         $('.top_box').not($('.top_box').eq(0)).css({"display": "none"});
+//         checkImg[index].src = "/lib/img/check_box.png";
+//         $(this).css({"background-color": "rgb(240, 240, 240)", "color": "black"});
+//         click_you= "카테취소";
+
+//     }
+
+
+
+//     sendit()
+// });
+
+
+const receiptList = document.querySelectorAll(".sorting_item")
+// 리스트 main_desc
+        const Sortingtit = document.querySelector(".main_desc") 
+        // 글자바뀌는곳 sorting_title
+        receiptList.forEach((target) => {
+            target.addEventListener("click", function() {
+                Sortingtit.sortingtit = event.target.textContent // 글자바꾸기
+                receiptList.forEach((e) => {
+                    // e.classList.remove("select_receipt") 
+                    // 글씨강조지우기
+                    e.childNodes[1].style.display="none" 
+                    // 체크표시지우기
+                })
+                // Sortingtit.classList.add("input_txt_color") 
+                // 글자바뀌는곳의 글씨색 변경
+                // target.classList.add("select_receipt") // 글씨강조추가
+                target.childNodes[1].style.display = "block" // 체크표시추가
+                dd_menu8.style.display="none" // 리스트창 닫기
+            })
+        })
+
 
 
 // 탑 고정 메뉴
@@ -135,30 +234,53 @@ function dd_menu8() {
 // https://liufeier.tistory.com/22 참고해서 할 것 !!
 
 
-var div2 = document.getElementsByIdName("top_menu01");
+// var div2 = document.getElementsById("top_menu01");
 
-    function handleClick(event) {
-        console.log(event.target);
-        // console.log(this);
-        // 콘솔창을 보면 둘다 동일한 값이 나온다
+//     function handleClick(event) {
+//         console.log(event.target);
+//         // console.log(this);
+//         // 콘솔창을 보면 둘다 동일한 값이 나온다
 
-        console.log(event.target.classList);
+//         console.log(event.target.classList);
 
-        if (event.target.classList[1] === "clicked") {
-        event.target.classList.remove("clicked");
-        } else {
-        for (var i = 0; i < div2.length; i++) {
-            div2[i].classList.remove("clicked");
-        }
+//         if (event.target.classList[1] === "clicked") {
+//         event.target.classList.remove("clicked");
+//         } else {
+//         for (var i = 0; i < div2.length; i++) {
+//             div2[i].classList.remove("clicked");
+//         }
 
-        event.target.classList.add("clicked");
-        }
-    }
+//         event.target.classList.add("clicked");
+//         }
+//     }
 
-    function init() {
-        for (var i = 0; i < div2.length; i++) {
-        div2[i].addEventListener("click", handleClick);
-        }
-    }
+//     function init() {
+//         for (var i = 0; i < div2.length; i++) {
+//         div2[i].addEventListener("click", handleClick);
+//         }
+//     }
 
-    init();
+//     init();
+
+// ===========================================
+// 버튼 클릭시 왔다 갔다 하는거
+// ===========================================
+
+// const nonClick = document.querySelectorAll(".non-click");
+// // .non-click의 선택자를 모두 호출
+
+// function handleClick(event) {
+//   // div에서 모든 "click" 클래스 제거
+//   nonClick.forEach((e) => {         // nonClick을 호출해서 forEach 돌리기
+//     e.classList.remove("click");
+//   });
+//   // 클릭한 div만 "click"클래스 추가
+//   event.target.classList.add("click");
+// }
+// ;
+
+// nonClick.forEach((e) => {
+//   e.addEventListener("click", handleClick);
+// });
+
+// ===========================================
