9348712caa50b1202f9a442fa8954664e53ac9a2
yunanjk <lyra082172@naver.com>
2023-01-05 01:26:39 +0900

    Product - ApiController, ApiRequest, ApiResponse, Repository, ApiLogicService

---
+++ shoekream/src/main/java/com/supreme/shoekream/model/network/response/ProductApiResponse.java
@@ -0,0 +1,29 @@
+package com.supreme.shoekream.model.network.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class ProductApiResponse {
+    private Long idx;
+    private String name;
+    private String nameKor;
+    private String img;
+    private Long brandIdx;
+    private String size;
+    private String categoryIdx;
+    private Long wishCount;
+    private String modelNum;
+    private String releaseDate;
+    private String color;
+    private LocalDateTime regDate;
+    private String createBy;
+
+}
