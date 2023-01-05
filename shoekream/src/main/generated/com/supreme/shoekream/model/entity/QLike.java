cf86aded1055e87e1d6c9ae1bcf24eb32448da0e
juy <61074722+juyeon1570@users.noreply.github.com>
2023-01-05 11:36:14 +0900

    푸터 a 연결

---
+++ shoekream/src/main/generated/com/supreme/shoekream/model/entity/QLike.java
@@ -0,0 +1,51 @@
+package com.supreme.shoekream.model.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QLike is a Querydsl query type for Like
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QLike extends EntityPathBase<Like> {
+
+    private static final long serialVersionUID = -1849101332L;
+
+    public static final QLike like = new QLike("like1");
+
+    public final com.supreme.shoekream.model.config.QBaseEntity _super = new com.supreme.shoekream.model.config.QBaseEntity(this);
+
+    public final NumberPath<Long> boardIdx = createNumber("boardIdx", Long.class);
+
+    public final DateTimePath<java.time.LocalDateTime> createdAt = createDateTime("createdAt", java.time.LocalDateTime.class);
+
+    public final NumberPath<Long> idx = createNumber("idx", Long.class);
+
+    public final NumberPath<Long> memberIdx = createNumber("memberIdx", Long.class);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> regDate = _super.regDate;
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> updateDate = _super.updateDate;
+
+    public QLike(String variable) {
+        super(Like.class, forVariable(variable));
+    }
+
+    public QLike(Path<? extends Like> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QLike(PathMetadata metadata) {
+        super(Like.class, metadata);
+    }
+
+}
+
---
+++ shoekream/src/main/generated/com/supreme/shoekream/model/entity/QTag.java
@@ -0,0 +1,49 @@
+package com.supreme.shoekream.model.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QTag is a Querydsl query type for Tag
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QTag extends EntityPathBase<Tag> {
+
+    private static final long serialVersionUID = -1860756315L;
+
+    public static final QTag tag = new QTag("tag");
+
+    public final com.supreme.shoekream.model.config.QBaseEntity _super = new com.supreme.shoekream.model.config.QBaseEntity(this);
+
+    public final NumberPath<Long> boardIdx = createNumber("boardIdx", Long.class);
+
+    public final NumberPath<Long> idx = createNumber("idx", Long.class);
+
+    public final NumberPath<Long> productIdx = createNumber("productIdx", Long.class);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> regDate = _super.regDate;
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> updateDate = _super.updateDate;
+
+    public QTag(String variable) {
+        super(Tag.class, forVariable(variable));
+    }
+
+    public QTag(Path<? extends Tag> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QTag(PathMetadata metadata) {
+        super(Tag.class, metadata);
+    }
+
+}
+
