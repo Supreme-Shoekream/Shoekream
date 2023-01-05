fedde76fe2e9d090258b3e3cdd2d5b771e80f526
sophia <sophiakim.dev@gmail.com>
2023-01-05 23:18:43 +0900

    Member added

---
+++ shoekream/src/main/java/com/supreme/shoekream/model/enumclass/Status.java
@@ -0,0 +1,16 @@
+package com.supreme.shoekream.model.enumclass;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum Status {
+    MEMBER(0, "일반회원", "사용자 가입상태"),
+    RESTRICTED(1, "정지", "사용자 탈퇴상태"),
+    SUSPENSION(1, "영구정지", "사용자 탈퇴상태");
+
+    private Integer id;
+    private String title;
+    private String description;
+}
