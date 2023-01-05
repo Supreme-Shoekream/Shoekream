807f34de1974a0b74f7dca4a7914be28fba281d8
yujaehyuk <fd730@naver.com>
2023-01-05 09:44:13 +0900

    1

---
+++ shoekream/src/main/generated/com/supreme/shoekream/model/entity/QBoard.java
@@ -0,0 +1,55 @@
+package com.supreme.shoekream.model.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QBoard is a Querydsl query type for Board
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QBoard extends EntityPathBase<Board> {
+
+    private static final long serialVersionUID = -1496632015L;
+
+    public static final QBoard board = new QBoard("board");
+
+    public final com.supreme.shoekream.model.config.QBaseEntity _super = new com.supreme.shoekream.model.config.QBaseEntity(this);
+
+    public final StringPath content = createString("content");
+
+    public final DateTimePath<java.time.LocalDateTime> createdAt = createDateTime("createdAt", java.time.LocalDateTime.class);
+
+    public final NumberPath<Long> idx = createNumber("idx", Long.class);
+
+    public final StringPath img = createString("img");
+
+    public final NumberPath<Long> memberIdx = createNumber("memberIdx", Long.class);
+
+    public final DateTimePath<java.time.LocalDateTime> modifiedAt = createDateTime("modifiedAt", java.time.LocalDateTime.class);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> regDate = _super.regDate;
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> updateDate = _super.updateDate;
+
+    public QBoard(String variable) {
+        super(Board.class, forVariable(variable));
+    }
+
+    public QBoard(Path<? extends Board> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QBoard(PathMetadata metadata) {
+        super(Board.class, metadata);
+    }
+
+}
+
--- shoekream/src/main/java/com/supreme/shoekream/repository/BrandRepository.java
+++ shoekream/src/main/java/com/supreme/shoekream/repository/BrandRepository.java
@@ -1,7 +1,8 @@
 package com.supreme.shoekream.repository;
 
 import com.supreme.shoekream.model.entity.Admin;
+import com.supreme.shoekream.model.entity.Brand;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BrandRepository extends JpaRepository<Admin, Long> {
+public interface BrandRepository extends JpaRepository<Brand, Long> {
 }
