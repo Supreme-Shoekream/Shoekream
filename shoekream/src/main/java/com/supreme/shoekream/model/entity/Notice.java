dab647afb4150a4b7b7e50b67cc36484bb6fc68d
lyh <jj2221@naver.com>
2023-01-05 17:43:49 +0900

    공지사항 entity 및 api 구현중

---
+++ shoekream/src/main/java/com/supreme/shoekream/model/entity/Notice.java
@@ -0,0 +1,32 @@
+package com.supreme.shoekream.model.entity;
+
+import com.supreme.shoekream.model.config.Auditable;
+import com.supreme.shoekream.model.config.BaseEntity;
+import com.supreme.shoekream.model.enumclass.UserStatus;
+import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+@EntityListeners(AuditingEntityListener.class)
+public class Notice extends BaseEntity implements Auditable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idx;
+    private String title;
+    private String content;
+    private String createdBy;
+    private LocalDateTime createdAt;
+    private String modifiedBy;
+    private LocalDateTime modifiedAt;
+
+}
