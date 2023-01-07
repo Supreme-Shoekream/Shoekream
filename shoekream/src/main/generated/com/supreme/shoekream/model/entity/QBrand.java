e96290ce4880dec383968162e9272efea057b052
jooeun <jje320594@gmail.com>
2023-01-08 01:13:51 +0900

    삭제 완료

---
+++ shoekream/src/main/generated/com/supreme/shoekream/model/entity/QBrand.java
@@ -0,0 +1,49 @@
+package com.supreme.shoekream.model.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QBrand is a Querydsl query type for Brand
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QBrand extends EntityPathBase<Brand> {
+
+    private static final long serialVersionUID = -1496542766L;
+
+    public static final QBrand brand = new QBrand("brand");
+
+    public final DateTimePath<java.time.LocalDateTime> createdAt = createDateTime("createdAt", java.time.LocalDateTime.class);
+
+    public final StringPath createdBy = createString("createdBy");
+
+    public final NumberPath<Long> idx = createNumber("idx", Long.class);
+
+    public final DateTimePath<java.time.LocalDateTime> modifiedAt = createDateTime("modifiedAt", java.time.LocalDateTime.class);
+
+    public final StringPath modifiedBy = createString("modifiedBy");
+
+    public final StringPath name = createString("name");
+
+    public final NumberPath<Long> totalConclusion = createNumber("totalConclusion", Long.class);
+
+    public QBrand(String variable) {
+        super(Brand.class, forVariable(variable));
+    }
+
+    public QBrand(Path<? extends Brand> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QBrand(PathMetadata metadata) {
+        super(Brand.class, metadata);
+    }
+
+}
+
