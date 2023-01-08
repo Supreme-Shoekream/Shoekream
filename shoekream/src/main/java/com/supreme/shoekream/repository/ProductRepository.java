c60564d086eacff1061f7ea6c1f4c752b8b61698
yunanjk <lyra082172@naver.com>
2023-01-09 01:09:45 +0900

    관리자페이지 - prodcucts crud 완성되기 직전

--- shoekream/src/main/java/com/supreme/shoekream/repository/ProductRepository.java
+++ shoekream/src/main/java/com/supreme/shoekream/repository/ProductRepository.java
@@ -1,8 +1,11 @@
 package com.supreme.shoekream.repository;
 
+import com.supreme.shoekream.model.entity.Admin;
 import com.supreme.shoekream.model.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
+import java.util.Optional;
 
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    Optional<Product> findByIdx(Long idx);
 }
