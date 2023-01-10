93d190893cf676f066153a881264190ce88f7b91
yunanjk <lyra082172@naver.com>
2023-01-11 08:58:17 +0900

    관리자페이지 체결내역 수정중

--- shoekream/src/main/java/com/supreme/shoekream/repository/ProductRepository.java
+++ shoekream/src/main/java/com/supreme/shoekream/repository/ProductRepository.java
@@ -1,11 +1,14 @@
 package com.supreme.shoekream.repository;
 
 import com.supreme.shoekream.model.entity.Admin;
+import com.supreme.shoekream.model.entity.Follow;
 import com.supreme.shoekream.model.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Optional<Product> findByIdx(Long idx);
+    List<Product> findByOrderByIdxDesc();
 }
