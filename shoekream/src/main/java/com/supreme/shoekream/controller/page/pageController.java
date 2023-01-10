fa22270f12ac4d01d87b77594c52937edb8b3931
jooeun <jje320594@gmail.com>
2023-01-10 23:07:47 +0900

    data.sql 구매/판매 내역 업로드 완료

--- shoekream/src/main/java/com/supreme/shoekream/controller/page/pageController.java
+++ shoekream/src/main/java/com/supreme/shoekream/controller/page/pageController.java
@@ -1,7 +1,12 @@
 package com.supreme.shoekream.controller.page;
 
+import com.supreme.shoekream.model.entity.Address;
 import com.supreme.shoekream.model.network.Header;
+import com.supreme.shoekream.model.network.response.MemberApiResponse;
 import com.supreme.shoekream.model.network.response.ProductApiResponse;
+import com.supreme.shoekream.service.AddressApiLogicService;
+import com.supreme.shoekream.service.CardApiLogicService;
+import com.supreme.shoekream.service.MemberApiLogicService;
 import com.supreme.shoekream.service.ProductApiLogicService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -20,7 +25,9 @@ import javax.servlet.http.HttpSession;
 public class pageController {
 
     private final ProductApiLogicService productApiLogicService;
-
+    private final MemberApiLogicService memberApiLogicService;
+    private final AddressApiLogicService addressApiLogicService;
+    private final CardApiLogicService cardApiLogicService;
 
     //보류- 이유: 상세페이지에서 자동으로 사이즈가 선택이 되어있는 상태로 product_idx가 넘어온다
     @GetMapping(path="buyselect/{idx}")   //http://localhost:8889/buyselect
@@ -52,23 +59,45 @@ public class pageController {
         System.out.println(map);
         //서비스로 데이터를 매개변수로 보낸다..?
         HttpSession session = request.getSession(false);
-        String id=null;
-        String name = null;
+        String idx=null;
+        String email = null;
         if(session== null){
             System.out.println("세션이 없습니다");
-//            return new ModelAndView("/login");
+//            return  "/login";
             return "/order/buycheck";
         }else{
-            id = (String) session.getAttribute("id");
-            name = (String) session.getAttribute("name");
+            idx = (String) session.getAttribute("idx");
+            email = (String) session.getAttribute("email");
             System.out.println("세션이 있습니다");
             return "/order/buycheck";
         }
     }
 
-    @GetMapping(path="buy")   //http://localhost:8889/buy
-    public ModelAndView buy(){
-        return new ModelAndView("/order/buy");
+    @GetMapping(path="buy/{productIdx}")   //http://localhost:8889/buy
+    public String buy(HttpServletRequest request,
+                            @PathVariable(name="productIdx",required = false) Long productIdx,
+                            ModelMap map){
+        Header<ProductApiResponse> product = productApiLogicService.read(productIdx);
+        map.addAttribute("product",product);    //상품정보 넣어서
+        System.out.println(map);
+        //세션값을 가정
+        Header<MemberApiResponse> member = memberApiLogicService.read(1L);
+        map.addAttribute("member",member);
+        System.out.println(member);
+//        Header<Address> address = addressApiLogicService.
+        HttpSession session = request.getSession(false);
+        String idx=null;
+        String email = null;
+        if(session== null){
+            System.out.println("세션이 없습니다");
+//            return  "/login";
+            return "/order/buy";
+        }else{
+            idx = (String) session.getAttribute("idx");
+            email = (String) session.getAttribute("email");
+            System.out.println("세션이 있습니다");
+            return "/order/buy";
+        }
     }   //viewName: 페이지이름이랑 같아야함
 
     @GetMapping(path = "buybidfinish")  //http://localhost:8889/buybidfinish
