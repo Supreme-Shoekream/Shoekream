5c4dd6b56747ad811b47453f062080489b7876bc
Sophie <sophiakim.dev@gmail.com>
2023-01-11 10:10:07 +0900

    test

--- shoekream/src/main/java/com/supreme/shoekream/controller/api/JoinApiController.java
+++ shoekream/src/main/java/com/supreme/shoekream/controller/api/JoinApiController.java
@@ -1,17 +1,22 @@
-//package com.supreme.shoekream.controller.api;
-//
-//import com.supreme.shoekream.controller.CrudController;
-//import com.supreme.shoekream.model.entity.Member;
-//import com.supreme.shoekream.model.network.Header;
-//import com.supreme.shoekream.model.network.request.MemberApiRequest;
-//import com.supreme.shoekream.model.network.response.MemberApiResponse;
-//import com.supreme.shoekream.service.MemberApiLogicService;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
+package com.supreme.shoekream.controller.api;
+
+import com.supreme.shoekream.controller.CrudController;
+import com.supreme.shoekream.model.entity.Member;
+import com.supreme.shoekream.model.network.Header;
+import com.supreme.shoekream.model.network.request.MemberApiRequest;
+import com.supreme.shoekream.model.network.response.MemberApiResponse;
+import com.supreme.shoekream.repository.MemberRepository;
+import com.supreme.shoekream.service.BaseService;
+import com.supreme.shoekream.service.MemberApiLogicService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.supreme.shoekream.model.entity.QMember.member;
+
 //@RestController
 //@RequestMapping("")    // http://localhost:9999
 //@RequiredArgsConstructor
