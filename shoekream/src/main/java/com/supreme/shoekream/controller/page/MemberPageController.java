be657ffcb4f144cb64757d5db2fac99706ac1a0e
Sophie <sophiakim.dev@gmail.com>
2023-01-19 14:58:56 +0900

    kakao login frontend

--- shoekream/src/main/java/com/supreme/shoekream/controller/page/MemberPageController.java
+++ shoekream/src/main/java/com/supreme/shoekream/controller/page/MemberPageController.java
@@ -3,9 +3,7 @@ package com.supreme.shoekream.controller.page;
 import com.supreme.shoekream.service.MemberApiLogicService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
@@ -42,6 +40,12 @@ public class MemberPageController {
         session.invalidate();
         return "redirect:/login";
     }
+
+//    @ResponseBody
+//    @GetMapping("/kakao")
+//    public void kakaoCallback(@RequestParam String code){
+//        System.out.println(code);
+//    }
 
     @GetMapping(path="join")   //http://localhost:8889/join
     public ModelAndView join(){
