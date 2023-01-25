18067e9a00e495509f5bb15b6ee26bea63f56e1b
yunanjk <lyra082172@naver.com>
2023-01-25 23:05:37 +0900

    상품 상세 체결내역 그래프(전체) + 즉시판매가 + 즉시구매가 구현 완료

--- shoekream/src/main/java/com/supreme/shoekream/controller/page/ProductPageController.java
+++ shoekream/src/main/java/com/supreme/shoekream/controller/page/ProductPageController.java
@@ -23,6 +23,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
@@ -86,5 +87,12 @@ public class ProductPageController {
 
 
     }
+
+
+//    public List<ConclusionDTO> graph(@PathVariable Long idx, ModelMap modelmap) {
+//        List<ConclusionDTO> conclusion = conclusionApiLogicService.con_read(idx);
+//        modelmap.addAttribute("conclusion", conclusion);
+//        return conclusion;
+//    }
 
 }
