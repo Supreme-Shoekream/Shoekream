f13b2f7501fc592e3fc888fc5d72aad271f9c37e
Sophie <sophiakim.dev@gmail.com>
2023-02-07 16:57:07 +0900

    added js faq policy

--- shoekream/src/main/resources/static/js/notice/faq.js
+++ shoekream/src/main/resources/static/js/notice/faq.js
@@ -24,8 +24,43 @@ $(".menu_item").click(function(){
     }); 
 
 // 테이블 선택
-$(".category_text").click(function(){
-    $(".category_text").removeClass("active");
+$(".category").click(function(){
+    $(".category").removeClass("active");
     $(this).addClass("active");
     $(".category").removeClass("category_on")
-    }); 
+    });
+
+// 필터링
+$(".category.policy").click(function(){
+    $(".dropdown.policy").css("display","block");
+    $(".dropdown.both").css("display","none");
+    $(".dropdown.buy").css("display","none");
+    $(".dropdown.sell").css("display","none");
+    });
+
+$(".category.both").click(function(){
+    $(".dropdown.both").css("display","block");
+    $(".dropdown.policy").css("display","none");
+    $(".dropdown.buy").css("display","none");
+    $(".dropdown.sell").css("display","none");
+});
+
+$(".category.buy").click(function(){
+    $(".dropdown.buy").css("display","block");
+    $(".dropdown.both").css("display","none");
+    $(".dropdown.policy").css("display","none");
+    $(".dropdown.sell").css("display","none");
+});
+
+$(".category.sell").click(function(){
+    $(".dropdown.sell").css("display","block");
+    $(".dropdown.buy").css("display","none");
+    $(".dropdown.both").css("display","none");
+    $(".dropdown.policy").css("display","none");
+});
+
+$(".category.total").click(function(){
+    $(".dropdown.buy").css("display","block");
+    $(".dropdown.both").css("display","block");
+    $(".dropdown.policy").css("display","block");
+});
