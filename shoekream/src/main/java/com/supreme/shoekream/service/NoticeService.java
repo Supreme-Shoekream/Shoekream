7722583cb64cc2cb9ff92b384eb6c3c19f17d2d4
jooeun <jje320594@gmail.com>
2023-02-08 21:42:06 +0900

    사용자 페이지 공지사항 리스트 상세보기

--- shoekream/src/main/java/com/supreme/shoekream/service/NoticeService.java
+++ shoekream/src/main/java/com/supreme/shoekream/service/NoticeService.java
@@ -1,7 +1,11 @@
 package com.supreme.shoekream.service;
 
+import com.supreme.shoekream.model.dto.NoticeDTO;
+import com.supreme.shoekream.repository.NoticeRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,4 +14,13 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Service
 public class NoticeService {
+
+    private final NoticeRepository noticeRepository;
+
+    public Page<NoticeDTO> list (Pageable pageable){
+        return noticeRepository.findAll(pageable).map(NoticeDTO::fromEntity);
+    }
+    public NoticeDTO read (Long idx){
+        return NoticeDTO.fromEntity(noticeRepository.findById(idx).get());
+    }
 }
