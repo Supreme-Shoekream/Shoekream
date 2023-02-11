77b50fd7f1b2cc90daa51e00bb17761af5307697
lyh <jj2221@naver.com>
2023-02-11 22:54:48 +0900

    메인 및 관리자 페이지 버그 수정

---
+++ admin/src/main/generated/com/supreme/admin/model/entity/QNotice.java
@@ -0,0 +1,47 @@
+package com.supreme.admin.model.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QNotice is a Querydsl query type for Notice
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QNotice extends EntityPathBase<Notice> {
+
+    private static final long serialVersionUID = 1314920381L;
+
+    public static final QNotice notice = new QNotice("notice");
+
+    public final com.supreme.admin.model.config.QBaseEntity _super = new com.supreme.admin.model.config.QBaseEntity(this);
+
+    public final StringPath content = createString("content");
+
+    public final DateTimePath<java.time.LocalDateTime> createdAt = createDateTime("createdAt", java.time.LocalDateTime.class);
+
+    public final NumberPath<Long> idx = createNumber("idx", Long.class);
+
+    public final DateTimePath<java.time.LocalDateTime> modifiedAt = createDateTime("modifiedAt", java.time.LocalDateTime.class);
+
+    public final StringPath title = createString("title");
+
+    public QNotice(String variable) {
+        super(Notice.class, forVariable(variable));
+    }
+
+    public QNotice(Path<? extends Notice> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QNotice(PathMetadata metadata) {
+        super(Notice.class, metadata);
+    }
+
+}
+
