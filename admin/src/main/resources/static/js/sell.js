387705e02cfe6026511e86a53024b7f25710c69d
jooeun <jje320594@gmail.com>
2023-02-12 20:13:00 +0900

    sell buy 상세 레이어 관리자 페이지 연결

--- admin/src/main/resources/static/js/sell.js
+++ admin/src/main/resources/static/js/sell.js
@@ -13,20 +13,31 @@ function layer_sell_view(idx){
         .then((response) => response.json())
         .then((data) => {
             console.log(data)
-            let member_idx = data.memberId;
-            if(member_idx == null) BuyIdx = "x"
+            let matching_idx = data.buyIdx;
+            if(matching_idx == null) {
+                matching_idx = "x"
+                document.querySelector('.layer_sell_view .buyer').innerHTML=matching_idx;
+            }else{
+                fetch('http://localhost:8899/api/order/buy/'+ matching_idx)
+                    .then((response) => response.json())
+                    .then((data2) => {
+                        matching_idx = data2.memberEmail;
+                        document.querySelector('.layer_sell_view .buyer').innerHTML=
+                            `<a href="/buy?page=0&searchKeyword=${matching_idx}">${matching_idx}</a>`
+                    })
+            }
             document.querySelector('.layer_sell_view .idx').innerHTML=data.idx;
             document.querySelector('.layer_sell_view .productIdx').innerHTML=data.productIdx;
             document.querySelector('.layer_sell_view .productName').innerHTML=data.productName;
-            document.querySelector('.layer_sell_view .price').innerHTML=data.price;
+            document.querySelector('.layer_sell_view .price').innerHTML=data.price.toLocaleString('ko-KR')+"원";
             document.querySelector('.layer_sell_view .productImg').src=data.productImg;
             document.querySelector('.layer_sell_view .productSize').innerHTML=data.productSize;
-            document.querySelector('.layer_sell_view .sender').innerHTML=data.sender;
+            document.querySelector('.layer_sell_view .seller').innerHTML=data.memberEmail;
             document.querySelector('.layer_sell_view .type').innerHTML=data.type;
             document.querySelector('.layer_sell_view .createdAt').innerHTML=data.createdAt;
             document.querySelector('.layer_sell_view .status').innerHTML=data.status;
             document.querySelector('.layer_sell_view .period').innerHTML=data.period;
-            document.querySelector('.layer_sell_view .name').innerHTML=data.name;
+
             document.querySelector('.layer_sell_view .totalPrice').innerHTML=data.totalPrice;
             document.querySelector('.layer_sell_view .fees').innerHTML=data.fees;
             document.querySelector('.layer_sell_view .account_idx').innerHTML=data.cardInfo;
